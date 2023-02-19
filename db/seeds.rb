b6814d04bbe7d18611f6e7cc4e5ad0a7e3f2b014
EC2 Default User <ec2-user@ip-172-31-36-228.ap-northeast-1.compute.internal>
2023-02-19 02:44:15 +0000

    add admin env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create(
-   email: 'admin@example.com',
-   password: 'adminuser'
+   email: ENV['ADMIN_USER'],
+   password: ENV['ADMIN_PASWORD']
 )
